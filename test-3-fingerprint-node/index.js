4914115f6c206856433608605f7f44c5509cff05
Arthur Pachachura <pachachura.arthur@gmail.com>
2016-08-05 11:54:12 -0500

    Working isFingerDown - note: all functions must be run in series

--- test-3-fingerprint-node/index.js
+++ test-3-fingerprint-node/index.js
@@ -1,18 +1,52 @@
 var GT511C3 = require('gt511c3');
 
-var fps = new GT511C3('/dev/ttyAMA0', { baudrate: 9600, debug: true });
+var fps = new GT511C3('/dev/ttyAMA0', { baudrate: 9600, debug: false });
 
 fps.init().then(
     function() {
         //init ok
-        console.log('firmware version: ' + fps.firmwareVersion);
-        console.log('iso area max: ' + fps.isoAreaMaxSize);
-        console.log('device serial number: ' + fps.deviceSerialNumber);
-	fps.ledONOFF(fps.LED_OFF);
+        console.log('Firmware version: ' + fps.firmwareVersion);
+        console.log('Iso area max: ' + fps.isoAreaMaxSize);
+        console.log('Device serial number: ' + fps.deviceSerialNumber);
+	fps.ledONOFF(fps.LED_ON);
+	fps.getEnrollCount().then(function(count) {
+  		console.log("Enrolled fingerprints count: " + count);
+		enroll();
+	});
+	//start();
     },
     function(err) {
         console.log('init err: ' + err);
     }
 );
 
-console.log("Ready!");
+function enroll()
+{
+	//fps.ledONOFF(fps.LED_ON);
+	fps.isPressFinger().then(function() {
+		console.log("Finger is down");
+	}, function(err) {
+		if (err !== undefined)
+			console.error(fps.decodeError(err));
+		else
+			console.log("Finger not down");
+		fps.ledONOFF(fps.LED_OFF);
+	});
+	/**fps.waitFinger(5000).then(function() {
+		console.log("Finger pressed!");
+		fps.waitReleaseFinger(10000).then(function() {
+			console.log("Finger released!");
+		}, function(err) {
+			console.error(fps.decodeError(err));
+		});
+	}, function(err) {
+		console.error(fps.decodeError(err));
+	});**/
+}
+
+function start()
+{
+    fps.ledONOFF(fps.LED_OFF);
+    while(!fps.isPressFinger()) { }
+    fps.ledONOFF(fps.LED_OFF);
+}
